680ced135661e4784afaf364c37caf441d3c57a1
Hanna Lytvynenko <97547547+Ani-k-a@users.noreply.github.com>
2023-07-03 21:17:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='goods-on-line-shop'>
+    <BrowserRouter basename='greenshop'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
